2ffc8f6105c79cd8a6846882b5d8a1e58249c872
AbdulMuqsit <abdulmuqsit987@gmail.com>
2019-12-31 12:59:12 +0500

    Partial-Front-End

---
+++ Assets/JS/home.js
@@ -0,0 +1,9 @@
+//mouse ent
+$('.dropdown').on('mouseover', '.dropdown', function(e) { 
+    $(this).children('.dropdown-toggle').dropdown('show');
+});
+
+// Mouse leave
+$('.dropdown').on('mouseleave', '.dropdown', function(e) { 
+    $(this).children('.dropdown-toggle').dropdown('hide');
+});
---
+++ Home.php
@@ -0,0 +1,133 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="Assets\CSS\home.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="css/animate.min.css" rel="stylesheet">
+    <link href="css/bootstrap-dropdownhover.min.css" rel="stylesheet">
+
+
+    <title>Online Shopping in Pakistan:Fashion,Electronics & Books-Daraz.pk</title>
+  </head>
+  <body>
+
+        <nav class="navbar navbar-expand-sm py-0 navbar-light " style="background-color:#F0F0F0;">
+                
+                 <div class="collapse navbar-collapse d-flex flex-row-reverse" id="navbarNavDropdown">
+                  <ul class="navbar-nav">
+                    <li class="nav-item active">
+                      <a class="nav-link py-0" href="#">SAVE MORE <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active py-0 ">
+                      <a class="nav-link py-0" href="#">SELL ON DARAZ</a>
+                    </li>
+                    <li class="nav-item active py-0">
+                      <a class="nav-link py-0" href="#">CUSTOMER CARE</a>
+                    </li>
+                    <li class="nav-item active py-0">
+                            <a class="nav-link py-0" href="#">TRACK MY ORDER</a>
+                          </li>
+                          <li class="nav-item active py-0">
+                                <a class="nav-link py-0" href="#">LOGIN</a>
+                              </li>      
+                              <li class="nav-item active py-0">
+                                    <a class="nav-link py-0" href="#">SIGNUP</a>
+                                  </li>           
+                     </ul>
+                </div>
+              </nav>
+
+              <nav class="navbar navbar-expand  navbar-light ">
+                
+                <div class="collapse navbar-collapse d-flex">
+                 <ul class="navbar-nav">
+                   <li class="nav-item pl-4">
+                     <a class="nav-link" href="#">
+                     <img src="Assets\IMG\logo.png" alt="DARAZ" height="41" width="136"> </a>
+                   </li>
+                   <li class="nav-item  pl-5 pr-0" >
+                   <a class="nav-link" href="#">
+                   <input type="text" id="box" class="form-control " placeholder="SEARCH ON DARAZ" ></a>
+                   </li>
+                   <li class="nav-item pl-0 col-auto" >
+                               <a class="nav-link" href="#"><button id="search" class="py-2 pr-3 pt-1" style="background-color:#FE701E;border:none;">
+                               <i class="fa fa-search pl-2 mt-2"></i></button></a>
+                             </li>     
+                             <li class="nav-item pl-2">
+                                   <a class="nav-link" href="#">
+                                   <i class='fa fa-shopping-cart' style='font-size:28px'></i></a>
+                                 </li>           
+                  <li class="nav-item pl-3">
+                  <a class="nav-link " href="#">
+                  <img src="Assets\IMG\home\daraz app download.png" alt="Download App" height="45" width="188"></a>
+                   </li>
+                    </ul>
+               </div>
+             </nav>
+
+
+<div id="carousel" class="carousel slide" data-ride="carousel">
+<ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1" class=""></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"class=""></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3"class=""></li>
+  </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="Assets\IMG\home\home slider image1.jpg" alt="First slide"height="342">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="Assets\IMG\home\home slider image2.webp" alt="Second slide"height="342">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="Assets\IMG\home\home slider image3.webp" alt="Third slide" height="342">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="Assets\IMG\home\home slider image4.webp" alt="Fourth slide" height="342">
+          </div>
+        </div>
+      </div>
+      
+      <div class="card-group">
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <script src="Assets\JS\home.js"></script>
+</body>
+</html>
